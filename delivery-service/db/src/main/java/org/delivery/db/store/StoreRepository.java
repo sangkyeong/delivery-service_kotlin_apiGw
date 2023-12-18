e44ec7672a6ecd03460bf0ca9288679d3a5c4460
sangKyeong <rlatkdrud123@naver.com>
2023-12-18 18:00:26 +0900

    231218 store db구현

---
+++ delivery-service/db/src/main/java/org/delivery/db/store/StoreRepository.java
@@ -0,0 +1,17 @@
+package org.delivery.db.store;
+
+import org.delivery.db.store.enums.StoreCategory;
+import org.delivery.db.store.enums.StoreStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+
+    Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, String status);
+
+    List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
+
+    List<StoreEntity> findAllByStatusAndCategoryOrderByStatusDesc(StoreStatus status, StoreCategory storeCategory);
+}
