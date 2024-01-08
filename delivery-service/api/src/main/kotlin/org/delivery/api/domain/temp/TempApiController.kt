e73af6f82e1975ec96b328c2b4c89004845a2672
sangKyeong <rlatkdrud123@naver.com>
2024-01-08 16:32:21 +0900

    240108 kotlin 컴파일을 위한 환경설정

---
+++ delivery-service/api/src/main/kotlin/org/delivery/api/domain/temp/TempApiController.kt
@@ -0,0 +1,16 @@
+package org.delivery.api.domain.temp
+
+import lombok.Getter
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/temp")
+class TempApiController {
+
+    @GetMapping("")
+    fun temp(): String{
+        return "hello"
+    }
+}
